7db029e1ee353a531cb3bd7f137a678af8413c6b
Your Name <you@example.com>
2020-10-05 16:17:16 -0500

    First Commit

---
+++ src/test/java/com/tecsup/lab3/controller/AppTest.java
@@ -0,0 +1,20 @@
+package com.tecsup.lab3.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
